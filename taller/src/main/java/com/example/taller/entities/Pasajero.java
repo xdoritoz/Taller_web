5619f210342a9b8491a4cee121d6baa6ae5c54d2
xdoritoz <123668559+xdoritoz@users.noreply.github.com>
2025-02-25 20:15:47 -0500

    Add files via upload

---
+++ taller/src/main/java/com/example/taller/entities/Pasajero.java
@@ -0,0 +1,24 @@
+package com.example.taller.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Data
+public class Pasajero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nombre;
+    private String NID;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "pasaporteID", nullable = false, referencedColumnName = "id")
+    private Pasaporte pasaporte;
+
+    @OneToMany(mappedBy = "pasajero")
+    private Set<Reserva> reservas;
+}
