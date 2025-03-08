163a927ed432127041386c1f299200c4f8b04a4d
xdoritoz <123668559+xdoritoz@users.noreply.github.com>
2025-03-07 19:18:39 -0500

    creating repositories package

--- src/test/java/com/example/taller/TallerApplicationTests.java
+++ src/test/java/com/example/taller/TallerApplicationTests.java
@@ -1,9 +1,16 @@
 package com.example.taller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.engine.discovery.predicates.IsPotentialTestContainer;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(IsPotentialTestContainer.class)
 @SpringBootTest
+
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TallerApplicationTests {
 
 	@Test
